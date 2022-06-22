d0576961aadc2fe3c8f7b5e58b11850446f38e54
Dizzi Tren <dizzitren@gmail.com>
2022-06-22 12:16:42 -0500

    first commit

---
+++ src/utilidadesJava/getdecimal/GetBigDecimal.java
@@ -0,0 +1,13 @@
+package utilidadesJava.getdecimal;
+
+import java.math.BigDecimal;
+
+public class GetBigDecimal {
+	
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Hola mundo");
+	}
+
+}
